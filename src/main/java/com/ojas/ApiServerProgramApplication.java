21bb72ce216df75ca5daf838d5e3d130db93900b
pb21071 <pb21071@OJAS-DD402.corp.ojas-it.com>
2021-03-25 21:27:56 +0530

    Added

---
+++ src/main/java/com/ojas/ApiServerProgramApplication.java
@@ -0,0 +1,13 @@
+package com.ojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerProgramApplication.class, args);
+	}
+
+}
